9b93be7e80fcb2d28505e0e3570e70a662edefcb
kmayya <kmayya@purdue.edu>
2022-12-03 15:55:40 -0500

    Added NavBar to sign in and sign out

---
+++ client/src/Pages/signin.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import '../App.css';
+import Nav from '../Components/Nav.js';
+import {Grid} from '@mui/material';
+
+    
+function SignIn() {
+
+    return (
+        <div className="App">
+            <Nav />
+            <Grid>            
+
+            </Grid>         
+
+        
+        </div>
+  );
+}
+
+export default SignIn;
---
+++ client/src/Pages/signup.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import '../App.css';
+import Nav from '../Components/Nav.js';
+import {Grid} from '@mui/material';
+
+    
+function SignUp() {
+
+    return (
+        <div className="App">
+            <Nav />
+            <Grid>            
+
+            </Grid>         
+
+        
+        </div>
+  );
+}
+
+export default SignUp;

fb24e91b914f9ffc4c5f68d50392940d6ed04c37
kmayya <kmayya@purdue.edu>
2022-12-02 20:13:57 -0500

    Added configuration file to access google cloud database

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import cfgReader
 from flask import Flask
 import pandas as pd
 import numpy as np
@@ -5,13 +6,12 @@ from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-PASSWORD ="moviestat123"
-PUBLIC_IP_ADDRESS ="34.28.217.152:3306"
-DBNAME ="movieDB"
-PROJECT_ID ="light-mountain-365203"
-INSTANCE_NAME ="movie348"
+config = cfgReader.read_config()
+PASSWORD = config['Cloud']['PASSWORD']
+PUBLIC_IP_ADDRESS = config['Cloud']['PUBLIC_IP_ADDRESS']
+DBNAME = config['Cloud']['DBNAME']
 
-url = f"mysql+pymysql://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?"#unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+url = f"mysql+pymysql://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?"
 app.config["SQLALCHEMY_DATABASE_URI"] = url
 engine = create_engine(url)
 connection = engine.connect()
---
+++ server/cfgReader.py
@@ -0,0 +1,5 @@
+import configparser
+def read_config():
+    config = configparser.ConfigParser()
+    config.read('configurations.ini')
+    return config

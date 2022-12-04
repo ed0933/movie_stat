3e41a07c192e593134b694ff1089d736eac363d3
ed0933 <82695344+ed0933@users.noreply.github.com>
2022-12-04 16:41:20 -0500

    Update app.py

--- server/app.py
+++ server/app.py
@@ -48,7 +48,10 @@ def getActorInfo():
 def insertUser():
     username = request.json.get('username')
     password = request.json.get('password')
-    movieQuery = f"insert into users Values('{username}', '{password}')"
+    movieQuery = f"if not exists(select username from users where username = :username)
+    begin
+        insert into users Values('{username}', '{password}')
+    end"
     engine.execute(text(movieQuery))
     connection.close()
     engine.dispose()

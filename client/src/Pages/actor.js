d123e7ae4867358b42ab597a3c65e0feb4c16ff3
rianna-parla <rparla@purdue.edu>
2022-12-04 17:07:34 -0500

    changed submit button to do nothingn for now

--- client/src/Pages/actor.js
+++ client/src/Pages/actor.js
@@ -19,14 +19,13 @@ class ActorLookup extends React.Component {
     console.log(this.state.value);
   }
 
-<<<<<<< HEAD
   render() {
     return (
       <div>
         <h1>Which actor do you want to look up?</h1>
         <Dropdown
           formLabel="Choose an actor"
-          buttonText="Send form"
+          buttonText="Submit form"
           onChange={this.handleSelect}          action=""
         >
           <Option value="Click to see options" />
@@ -35,31 +34,6 @@ class ActorLookup extends React.Component {
           <Option value="Actor 3" />
         </Dropdown>
         <p>You selected {this.state.value} </p>
-=======
-
-
-  return (
-
-      <div className="actor">
-      <div className="actor-form">
-        <Paper style={styles.paperContainer}>
-        <Grid container spacing={0} position= 'fixed' direction='column' alignItems='center' justifyContent='left' style={{ minHeight: '100%', backdropFilter: 'blur(10px)',  fontFamily: 'Trebuchet MS', fontSize: '4vh', color: 'white', textShadow: "2px 2px #6e6666", opacity:"0.90"}}>
-        <h1>Search a movie with actor name!</h1>
-        <form>  
-        <b> Choose the actor: </b>  
-        <select id = "Actorname" onchange = "ListActors()" > 
-        <option> ---Choose Actor--- </option>  
-        <option> a </option>  
-        <option> b </option>  
-        <option> c </option>  
-        <option> d </option>  
-        </select> 
-         
-        {isSubmitted ? <div>Actor Looked Up!</div> : renderForm}
-        </form> 
-        </Grid>
-      </Paper>
->>>>>>> 92376fc79ce183819cd26eb8cb3d8eb073ceefc7
       </div>
     );
   }

42094b8dca05b468dfdafc152dd356be93d51ba4
kmayya <kmayya@purdue.edu>
2022-12-09 18:13:10 -0500

    actorLookup changes

--- client/src/Pages/actor.js
+++ client/src/Pages/actor.js
@@ -1,9 +1,12 @@
 import 'devextreme/dist/css/dx.common.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import 'devextreme/dist/css/dx.light.css'; //npx -p devextreme-cli devextreme add devextreme-react
 import './actor.css';
+import apiService from '../Components/apiService.js'
 
-import {Paper, Grid} from '@mui/material'; //npm install @mui/material @emotion/react @emotion/styled
+
+import {Paper, Grid, Box} from '@mui/material'; //npm install @mui/material @emotion/react @emotion/styled
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Image from '../Assets/img2.jpg';
 
 const styles = {
@@ -18,19 +21,28 @@ const styles = {
 function ActorLookup() {
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formJSON, setFormJSON] = useState({"stage" : {"0": "None"}, "dow" : {"0": "None"}, "stage" : {"0": "None"}, "birth" : {"0": "None"}, "gov" : {"0": "None"}, "giv" : {"0": "None"}, "gen" : {"0": "None"}, "dob" : {"0": "None"}})
+  const [formResult, setFormResult] = useState('');
+
+  useEffect(() => {
+    setFormResult("Stage Name [" + JSON.stringify(formJSON["stage"][0]) + "]: " + "Work Years [" + JSON.stringify(formJSON["dow"][0]) + "]: " + "Birth Name [" + JSON.stringify(formJSON["birth"][0]) + ", " + JSON.stringify(formJSON["gov"][0]) + "]: ");
+  }, [formJSON]);
+
   const errors = {
-    uname: "Please Enter Actor",
+    stage: "Please Enter Actor",
   };
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
 
-    var { uname } = document.forms[0];
-    if (uname.value === "") {
-      setErrorMessages({ name: "uname", message: errors.uname });
+    var { stage } = document.forms[0];
+    if (stage.value === "") {
+      setErrorMessages({ name: "stage", message: errors.stage });
   } else {
       setIsSubmitted(true); 
   }
+  apiService.GetActorInfo(stage.value).then((response) => setFormJSON(response))
+    .catch(error => console.log('error', error));
 };
 
 const renderErrorMessage = (name) =>
@@ -42,8 +54,8 @@ const renderErrorMessage = (name) =>
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Enter actor name</label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+          <input type="text" name="stage" required />
+          {renderErrorMessage("stage")}
         </div>
         <div className="button-container">
           <input type="submit" />
@@ -59,6 +71,28 @@ const renderErrorMessage = (name) =>
       <Grid container spacing={0} position= 'fixed' direction='column' alignItems='center' justifyContent='top' style={{ minHeight: '100%', backdropFilter: 'blur(10px)',  fontFamily: 'Trebuchet MS', fontSize: '4vh', color: 'white', textShadow: "2px 2px #6e6666", opacity:"0.90"}}>
       <h1>Search an actor</h1>
       {isSubmitted ? <div>Actor Looked Up!!</div> : renderForm}
+      </Grid>
+
+      <Grid container spacing={2} position= 'fixed' direction='row' alignItems='center' marginLeft= '3vh' marginTop = '300px' justifyContent='flex-start'>            
+        <Grid item>
+                <ArrowForwardIosIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }} />
+        </Grid>
+        <Grid item> 
+            <Box component="div" sx={{ display: 'inline', 
+                    px: 20,
+                    py: 1, 
+                    m: 0.1, 
+                    bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+                    color: (theme) => theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+                    border: '1px solid',
+                    borderColor: (theme) => theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+                    borderRadius: 2,
+                    fontSize: '1rem',
+                    fontWeight: '700',
+                }}>
+                {formResult}
+            </Box>
+          </Grid>
       </Grid>
     </Paper>
     </div>
